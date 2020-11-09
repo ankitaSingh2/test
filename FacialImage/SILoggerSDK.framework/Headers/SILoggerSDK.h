6ecff150d52d81664d8fb53b5e1b66bed07de7de
Ankita Singh <Ankita.singh2@unisys.com>
2020-11-09 16:52:32 +0530

    init

---
+++ FacialImage/SILoggerSDK.framework/Headers/SILoggerSDK.h
@@ -0,0 +1,19 @@
+//
+//  SILoggerSDK.h
+//  SILoggerSDK
+//
+//  Created by Mohit on 05/05/20.
+//  Copyright © 2020 Unisys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SILoggerSDK.
+FOUNDATION_EXPORT double SILoggerSDKVersionNumber;
+
+//! Project version string for SILoggerSDK.
+FOUNDATION_EXPORT const unsigned char SILoggerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SILoggerSDK/PublicHeader.h>
+
+
