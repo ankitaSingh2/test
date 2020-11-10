cf532af45a8e0e01f93bd5fcfc40e05e9f725a8b
Ankita Singh <Ankita.singh2@unisys.com>
2020-11-10 12:30:47 +0530

    fetched parsing data

--- Techtest/ViewController.swift
+++ Techtest/ViewController.swift
@@ -9,14 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
-        self.title = "FirstVC"
+        gettingdata()
         // Do any additional setup after loading the view.
     }
-
-
+    func gettingdata()
+    {
+        let url = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+        URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
+            
+            if let d = data {
+                if let value = String(data: d, encoding: String.Encoding.ascii) {
+                    
+                    if let jsonData = value.data(using: String.Encoding.utf8) {
+                        do {
+                            let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as! [String: Any]
+                            DispatchQueue.main.async {
+                            if let title = json["title"] as? String
+                            {
+                                self.title = title
+                            }
+                            if let arr = json["rows"] as? [[String: Any]] {
+                                debugPrint(arr)
+                            }
+                            }
+                            
+                        } catch {
+                            NSLog("ERROR \(error.localizedDescription)")
+                        }
+                    }
+                }
+                
+            }
+        }.resume()
+    }
+    
+    
 }
 
