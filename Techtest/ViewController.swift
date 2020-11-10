ccce0ac20fdd5d405b61b83df17a351a8589ca01
Ankita Singh <Ankita.singh2@unisys.com>
2020-11-10 11:39:10 +0530

    init

---
+++ Techtest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Techtest
+//
+//  Created by Ankita on 10/11/20.
+//  Copyright © 2020 Ankita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        self.title = "FirstVC"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
